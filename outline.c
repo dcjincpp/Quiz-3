4a97f35044940e72a5c3a457358a6664724e077d
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-19 20:27:34 -0700

    Outline for how to do part 1a and b

---
+++ outline.c
@@ -0,0 +1,5 @@
+//create function for find by phone in employeeOne and test with two methods one matching one not matching in employeeMain
+//create function for find by salary in employeeOne and test with two methods one matching one not matching in employeeMain
+//create function for find by phone in employeeTwo and test with two methods one matching one not matching in employeeMain created in previous step
+//create function for find by salary in employeeTwo and test with two methods one matching one not matching in employeeMain created in previous step
+
