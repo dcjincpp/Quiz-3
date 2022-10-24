b0cf978ea290da8da9c1722243dc50faa5f3d1af
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 18:21:48 -0700

    Randomly chooses food item

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <time.h> 
 
 int main( int argc, double argv[])
 {
+    time_t i;
+    srand((unsigned) time(&i)); 
+    
     double tax = argv[0] / 100;
     double tip = argv[1] / 100;
     double subTotal;
@@ -10,6 +15,28 @@ int main( int argc, double argv[])
     double soup = 4.55;
     double sandwich = 13.25;
     double pizza = 22.35;
+    int item = (rand() % 4);
+
+    switch (item)
+    {
+        case 0:
+            subTotal = 9.95;
+            break;
+        
+        case 1:
+            subTotal = 4.55;
+            break;
+        
+        case 2:
+            subTotal = 13.25;
+            break;
+        
+        case 3:
+            subTotal = 22.35;
+            break;
+
+        default: break;
+    }
 
     printf("Tax: %lf Tip: %lf\n", tax, tip);
     printf("Subtotal: %lf\n", subTotal);
