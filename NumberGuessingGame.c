483795153e4bc8480fb080275ad9d6e98af2ec13
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 18:56:10 -0700

    Takes an input

--- NumberGuessingGame.c
+++ NumberGuessingGame.c
@@ -8,7 +8,11 @@ int main()
     srand((unsigned) time(&t));
     int number = ((rand() % 10) + 1);
 
+    int option;
+
     printf("Press 1 to play a game\n");
     printf("Press 2 to change the max number\n");
     printf("Press 3 to quit\n");
+
+    scanf("%d", &option);
 }
