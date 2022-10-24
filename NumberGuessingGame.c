b3c313aa9c64019f30d8a0a23b700f9b0ccd82af
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 20:23:48 -0700

    Added option 2

--- NumberGuessingGame.c
+++ NumberGuessingGame.c
@@ -7,16 +7,19 @@ int main()
 {
     time_t t;
     srand((unsigned) time(&t));
-    int number = ((rand() % 10) + 1);
+    int max = 10;
     int guess;
     bool correct = false;
+    bool quit = false;
 
     int option;
 
-    //while()
+    //while(!quit)
+    int number = ((rand() % max) + 1);
     printf("Press 1 to play a game\n");
     printf("Press 2 to change the max number\n");
     printf("Press 3 to quit\n");
+
 
     scanf("%d", &option);
 
@@ -24,7 +27,7 @@ int main()
     {
         case 1:
             printf("You can enter q to quit anytime\n");
-            printf("Enter a number: ");
+            printf("Enter a number or press q to quit: ");
             while(!correct && (scanf("%d", &guess) == 1))
                 if (guess < number)
                 {
@@ -35,11 +38,30 @@ int main()
                     printf("Your guess is too high. Try again\n");
                     //scanf("%d", &guess);
                 } else if (guess == number) {
-                    printf("You won!");
+                    printf("You won!\n");
                     correct = true;
                 }
             break;
-    }
 
+        case 2:
+            printf("You can change the max value only up to 100 and can not lower than 1: ");
+            scanf("%d", &max);
+            while(max > 100 || max < 1)
+            {
+            if(max > 100)
+            {
+                printf("The value is past the maximum value, enter another number\n");
+                scanf("%d", &max);
+            } else if (max < 0 ) {
+                printf("The max can not be negative, enter another number\n");
+                scanf("%d", &max);
+            } else if (max == 0) {
+                printf("The max can not be 0, enter another number\n");
+                scanf("%d", &max);
+            }
+            }
+            break;
+
+    }
     return EXIT_SUCCESS;
 }
