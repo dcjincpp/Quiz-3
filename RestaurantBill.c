0db84cad8427f4bd6372a9a577fd3c9988e08df7
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 18:07:56 -0700

    Created output for tax and tip

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main( int argc, char *argv[])
+int main( int argc, double argv[])
 {
+    double tax = argv[0] / 100;
+    double tip = argv[1] / 100;
 
+    printf("Tax: %lf Tip: %lf", tax, tip);
 }
