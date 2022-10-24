3352987e978207f4046cbd7947376a580bea0492
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 19:27:06 -0700

    Created the game for option 1 but can not quit

--- NumberGuessingGame.c
+++ NumberGuessingGame.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h> 
-#include <time.h> 
+#include <time.h>
+#include <stdbool.h>
 
 int main()
 {
     time_t t;
     srand((unsigned) time(&t));
     int number = ((rand() % 10) + 1);
+    int guess;
+    bool correct = false;
 
     int option;
 
+    //while()
     printf("Press 1 to play a game\n");
     printf("Press 2 to change the max number\n");
     printf("Press 3 to quit\n");
 
     scanf("%d", &option);
+
+    switch (option)
+    {
+        case 1:
+            printf("You can enter q to quit anytime\n");
+            printf("Enter a number: ");
+            scanf("%d", &guess);
+            while(!correct)
+                if (guess < number)
+                {
+                    
+                    printf("Your guess is too low. Try again\n");
+                    scanf("%d", &guess);
+                } else if (guess > number) {
+                    printf("Your guess is too high. Try again\n");
+                    scanf("%d", &guess);
+                } else if (guess == number) {
+                    printf("You won!");
+                    correct = true;
+                }
+            break;
+    }
+
+    return EXIT_SUCCESS;
 }
