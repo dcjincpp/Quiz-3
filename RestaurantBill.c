c44466b382f2a9bc017a640fe62fd46ffec06ce3
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 18:12:28 -0700

    Created subtotal and total output

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -4,6 +4,10 @@ int main( int argc, double argv[])
 {
     double tax = argv[0] / 100;
     double tip = argv[1] / 100;
+    double subTotal;
+    double total;
 
-    printf("Tax: %lf Tip: %lf", tax, tip);
+    printf("Tax: %lf Tip: %lf\n", tax, tip);
+    printf("Subtotal: %lf\n", subTotal);
+    printf("Total: %lf", total);
 }
