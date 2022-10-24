dd7a54e54138196f96861c04a68e7729aaa1b13f
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 23:05:27 -0700

    Checks that the numbers in the square are between 1-9

--- LoShuMagicSquare.c
+++ LoShuMagicSquare.c
@@ -4,7 +4,9 @@
 
 void checkMagicSquare(int square[3][3])
 {
-    int a;
+    bool unique = false;
+    bool range = false;
+    int a, b, c;
     int row1 = 0;
     int row2 = 0;
     int row3 = 0;
@@ -14,25 +16,39 @@ void checkMagicSquare(int square[3][3])
     int diagonal1 = 0;
     int diagonal2 = 0;
 
-
-
-    for(a = 0; a < 3; a++)
+    for(b = 0; b < 3; b++)
     {
-        diagonal1 += square[a][a];
-        diagonal2 += square[2-a][a];
-        row1 += square[0][a];
-        row2 += square[1][a];
-        row3 += square[2][a];
-        column1 += square[a][0];
-        column2 += square[a][1];
-        column3 += square[a][2];
+        for(c = 0; c < 3; c++)
+        {
+            if((square[b][c] > 0) && (square[b][c] < 10))
+            {
+                range = true;
+            } else {
+                printf("%d is not between 1 and 9\n", square[b][c]);
+            }
+        }
     }
 
-    if ((row1 == row2) && (row3 == column1) && (column2 == column3) && (diagonal1 == diagonal2) && (row2 == row3) && (column1 == column2) && (column3 == diagonal1))
+    if(unique && range)
     {
-        printf("Is a magic square");
-    } else {
-        printf("Is not a magic square");
+        for(a = 0; a < 3; a++)
+        {
+            diagonal1 += square[a][a];
+            diagonal2 += square[2-a][a];
+            row1 += square[0][a];
+            row2 += square[1][a];
+            row3 += square[2][a];
+            column1 += square[a][0];
+            column2 += square[a][1];
+            column3 += square[a][2];
+        }
+
+        if ((row1 == row2) && (row3 == column1) && (column2 == column3) && (diagonal1 == diagonal2) && (row2 == row3) && (column1 == column2) && (column3 == diagonal1))
+        {
+            printf("Is a magic square");
+        } else {
+            printf("Is not a magic square");
+        }
     }
 
 }
@@ -42,7 +58,6 @@ int main()
     int test[3][3] = {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}};
 
     checkMagicSquare(test);
-
 
     return EXIT_SUCCESS;
 }
