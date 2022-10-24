c189171ca650f2ff2338ae76cbd4fe95d7f6c530
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 18:14:06 -0700

    Created food items and their cost

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -6,6 +6,10 @@ int main( int argc, double argv[])
     double tip = argv[1] / 100;
     double subTotal;
     double total;
+    double salad = 9.95;
+    double soup = 4.55;
+    double sandwich = 13.25;
+    double pizza = 22.35;
 
     printf("Tax: %lf Tip: %lf\n", tax, tip);
     printf("Subtotal: %lf\n", subTotal);
