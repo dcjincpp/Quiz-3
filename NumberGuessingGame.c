6c87f320c84bd4e5648052d2d17a9da7081577e4
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-23 18:52:53 -0700

    Prompts user to choose between 3 options

--- NumberGuessingGame.c
+++ NumberGuessingGame.c
@@ -7,4 +7,8 @@ int main()
     time_t t;
     srand((unsigned) time(&t));
     int number = ((rand() % 10) + 1);
+
+    printf("Press 1 to play a game\n");
+    printf("Press 2 to change the max number\n");
+    printf("Press 3 to quit\n");
 }
